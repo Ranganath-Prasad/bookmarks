5f651a554153c626ea5eab0ed6d226c4da2b336b
Ranganath Prasad U M <Ranganath_Prasad_U_M@dell.com>
2022-01-02 04:32:26 +0530

    Initial commit

---
+++ data.js
@@ -0,0 +1,35 @@
+var data = {"data": [
+    {
+    "technology" : "csharp",
+    "topic":"Collections",
+    "post":"AsEnumerable Vs AsQueryable",
+    "complexity": "Basic",
+    "link": "http://google.com",
+    "linkType": "video"
+    },
+     {
+    "technology" : "angular",
+    "topic":"Routing",
+    "post":"Angular Routes - 1",
+    "complexity": "Basic",
+    "link": "http://google.com",
+    "linkType": "video"
+    },
+      {
+    "technology" : "angular",
+    "topic":"Routing",
+    "post":"Angular Routes - 2",
+    "complexity": "Intermediate",
+    "link": "http://google.com",
+    "linkType": "video"
+    },
+        {
+    "technology" : "angular",
+    "topic":"Routing",
+    "post":"Angular Routes - 3",
+    "complexity": "Intermediate",
+    "link": "http://google.com",
+    "linkType": "video"
+    }
+    ]
+}
